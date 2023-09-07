fce52db835ad1e1b0aff4884e6a5ade6218ecb49
Fannn03 <fann.fann771@gmail.com>
2023-09-07 20:12:48 +0700

    add new line

--- config/logger.js
+++ config/logger.js
@@ -3,10 +3,9 @@ const moment = require('moment')
 moment.locale()
 
 module.exports = (req, res, messages) => {
-    console.log(res.body);
 
     const time = moment().format("H:mm:ss")
-    const content = `[${time}] ${req.method} : ${req.originalUrl} | ${res.statusCode} : ${JSON.stringify(messages)}`
+    const content = `[${time}] ${req.method} : ${req.originalUrl} | ${res.statusCode} : ${JSON.stringify(messages)}\n`
     return fs.appendFile('./logs/request.log', content, (err) => {
         if(err) throw new Error(err)
     })

92329e29c3e2ff6d145957ec9b2b59f3a2770e39
Fannn03 <fann.fann771@gmail.com>
2023-09-07 19:56:46 +0700

    add messages response

--- config/logger.js
+++ config/logger.js
@@ -2,11 +2,11 @@ const fs = require('fs')
 const moment = require('moment')
 moment.locale()
 
-module.exports = (req, res) => {
+module.exports = (req, res, messages) => {
+    console.log(res.body);
 
     const time = moment().format("H:mm:ss")
-    const content = `[${time}] ${req.method} : ${req.originalUrl} | ${res.statusCode} : ${JSON.stringify(res.body.message)}`
-
+    const content = `[${time}] ${req.method} : ${req.originalUrl} | ${res.statusCode} : ${JSON.stringify(messages)}`
     return fs.appendFile('./logs/request.log', content, (err) => {
         if(err) throw new Error(err)
     })

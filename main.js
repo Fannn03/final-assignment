ca844a96104db84a5c2217455c0cb85cd2537e0f
Fannn03 <fann.fann771@gmail.com>
2023-09-07 19:21:33 +0700

    create auth router

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const express = require('express')
 require('dotenv').config()
+const AuthRouter = require('./routes/AuthRouter')
 
 const app = express()
 const env = process.env
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use(AuthRouter)
 
 app.listen(env.SERVER_PORT, () => {
     console.log('server started');

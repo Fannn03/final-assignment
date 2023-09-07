6faa84d45d181ead8891890f564140b19a43cc52
Fannn03 <fann.fann771@gmail.com>
2023-09-07 20:04:27 +0700

    custom validator

--- models/UserSchema.js
+++ models/UserSchema.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const Schema = new mongoose.Schema({
     username: {
         type: String,
+        index: true,
         unique: true,
         required: [true, 'username tidak boleh kosong'],
         minLength: [4, 'username tidak boleh kurang dari 4 karakter'],
-        maxLength: [12, 'username tidak boleh lebih dari 12 karakter']
+        maxLength: [12, 'username tidak boleh lebih dari 12 karakter'],
+        validate: {
+            validator: (value) => {
+                return /^\S*$/.test(value)
+            },
+            message: () => 'username tidak boleh mengandung spasi'
+        }
     },
     password: {
         type: String,
@@ -14,10 +22,12 @@ const Schema = new mongoose.Schema({
     }
 })
 
-Schema.pre('save', function(next){
+Schema.pre('save', async function(next){
 
     if(this.password.length < 4) throw new Error('password tidak boleh kurang dari 4 karakter')
     if(this.password.length > 16) throw new Error('password tidak boleh lebih dari 16 karakter')
+
+    this.password = await bcrypt.hash(this.password, 10)
 
     return next()
 

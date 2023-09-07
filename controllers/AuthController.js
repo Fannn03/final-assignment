8368c92afb922e11bc99e3501d89e688d81fe59f
Fannn03 <fann.fann771@gmail.com>
2023-09-07 20:12:58 +0700

    catch error message

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,58 @@
+const logger = require("../config/logger")
+const UserSchema = require("../models/UserSchema")
+
 module.exports = {
     register: async (req, res) => {
-        res.send('a')
+        
+        const {username, password} = req.body
+
+        const user =  new UserSchema({
+            username: username,
+            password: password
+        })
+
+        try {
+            await user.save()
+        }catch (err) {
+
+            if(err.code === 11000) {
+                let messages = 'username telah dipakai'
+                
+                res.status(400).json({
+                    result: 'error',
+                    messages: messages
+                })
+    
+                return logger(req, res, messages)
+            }
+
+            let messages = {}
+
+            if(err.errors) {
+                const objValues = Object.values(err.errors)
+                objValues.map(error => {
+                    messages[error.path] = error.message 
+                })
+            }else{
+                messages.error = err.message
+            }
+
+            res.status(400).json({
+                result: 'error',
+                messages: messages
+            })
+
+            return logger(req, res, messages)
+
+        }
+
+        res.status(200).json({
+            result: 'success',
+            messages: 'Berhasil mendaftarkan user',
+            data: user
+        })
+
+        return logger(req, res, 'Berhasil mendaftarkan user')
+
     }
 }
